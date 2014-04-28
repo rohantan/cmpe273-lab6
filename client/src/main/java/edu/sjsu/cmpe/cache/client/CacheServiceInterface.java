10c79c6b19d88c2a398545262e35d74313abfb41
Rohan <rohan.tan@gmail.com>
2014-04-27 21:10:40 -0700

    consistent Hashing code code

--- client/src/main/java/edu/sjsu/cmpe/cache/client/CacheServiceInterface.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/CacheServiceInterface.java
@@ -6,6 +6,5 @@ package edu.sjsu.cmpe.cache.client;
  */
 public interface CacheServiceInterface {
     public String get(long key);
-
     public void put(long key, String value);
 }
--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,19 +1,44 @@
 package edu.sjsu.cmpe.cache.client;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.google.common.hash.Hashing;
+
 public class Client {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
+	public static void main(String[] args) throws Exception {
+		System.out.println("Starting Cache Client...");
+		int bucket;
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
+		List<CacheServiceInterface> serverlist = new ArrayList<CacheServiceInterface>();
+		serverlist.add(new DistributedCacheService("http://localhost:3000"));
+		serverlist.add(new DistributedCacheService("http://localhost:3001"));
+		serverlist.add(new DistributedCacheService("http://localhost:3002"));
 
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
+		CharSequence val;
+		for(int key=1;key<=10;key++)
+		{
+			val =generateRandomChar();
+			bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(key)), serverlist.size());
+			serverlist.get(bucket).put(key, val.toString());
 
-        System.out.println("Existing Cache Client...");
-    }
+			System.out.println("put (key "+key +" and value "+ val+")" +" in bucket "+bucket);
+		}
+		for(int key=1;key<=10;key++)
+		{
+			bucket = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(key)), serverlist.size());
+			System.out.println("get (key "+key +" and value "+ serverlist.get(bucket).get(key) +")" + "from bucket " + bucket);
+		}
+
+		System.out.println("Exiting Cache Client...");
+	}
+
+	public static CharSequence generateRandomChar(){
+		String alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		int character=(int)(Math.random()*26);
+		CharSequence s=alphabet.substring(character, character+1);
+		return s;
+	}
 
 }
